88719de51462b651edac89d5de84add473a169ba
Stephen Shank <sshank314@gmail.com>
2018-01-22 13:47:16 -0500

    Initial attempt to use enter with partial rendering for scrolling.

--- src/alignment.js
+++ src/alignment.js
@@ -6,10 +6,12 @@ import fastaParser from './fasta';
 
 class Alignment extends Component {
   renderAlignment(){
-    var sequences = fastaParser(this.props.fasta);
+    var parsed = fastaParser(this.props.fasta),
+      {characters, names} = parsed;
+
     var margin = {top: 20, right: 10, bottom: 20, left: 10},
-        number_of_characters = sequences[0].seq.length,
-        number_of_sequences = sequences.length,
+        number_of_sequences = names.length,
+        number_of_characters = characters.length/number_of_sequences,
         character_size = 20,
         labels_width = 200,
         width = labels_width + number_of_characters*character_size - margin.left - margin.right,
@@ -29,7 +31,7 @@ class Alignment extends Component {
     var labels = svg.append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
     
-    var characters = svg.append('g')
+    var character_group = svg.append('g')
         .attr('transform', 'translate(' + (labels_width + margin.left) + ',' + margin.top + ')')
         .style('width', '500px')
         .style('overflow-x', 'scroll');
@@ -51,29 +53,48 @@ class Alignment extends Component {
       .attr('transform', 'translate(' + (margin.left+labels_width+site_scale(.5)) + ',' + margin.top + ')')
       .call(axis)
     
-    for(var i=0; i < number_of_sequences; i++){
-      labels.append('text')
-        .attr('transform', 'translate(0,' + label_scale(i+.75) + ')')
-        .text(sequences[i].name);
-      for(var j=0; j < number_of_characters; j++){
-        var g_character = characters.append("g")
-          .attr('transform', 'translate(' + site_scale(j) + ',' + label_scale(i) + ')');
-        var character = sequences[i].seq[j];
-         
-        g_character.append('rect')
-          .attr('x', 0)
-          .attr('y', 0)
-          .attr('width', site_scale(1))
-          .attr('height', site_scale(1))
-          .attr('fill', colors[character] || 'red')
+    var character_gs = character_group.selectAll('g')
+      .data(characters.filter(d=>d.j < 100))
+      .enter()
+      .append('g')
+        .attr('transform', d=>'translate(' + site_scale(d.j) + ',' + label_scale(d.i) + ')');
+
+    character_gs.append('rect')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('width', site_scale(1))
+      .attr('height', site_scale(1))
+      .attr('fill', d=>colors[d.char] || 'red');
         
-        g_character.append('text')
-          .attr('transform', 'translate(' + site_scale(.5) + ',' + site_scale(.5) + ')')
-          .attr('text-anchor', 'middle')
-          .attr('dy', '.25em')
-          .text(character)
-      }
-    }
+    character_gs.append('text')
+      .attr('transform', 'translate(' + site_scale(.5) + ',' + site_scale(.5) + ')')
+      .attr('text-anchor', 'middle')
+      .attr('dy', '.25em')
+      .text(d=>d.char);
+
+//    for(var i=0; i < number_of_sequences; i++){
+//      labels.append('text')
+//        .attr('transform', 'translate(0,' + label_scale(i+.75) + ')')
+//        .text(sequences[i].name);
+//      for(var j=0; j < number_of_characters; j++){
+//        var g_character = characters.append("g")
+//          .attr('transform', 'translate(' + site_scale(j) + ',' + label_scale(i) + ')');
+//        var character = sequences[i].seq[j];
+//         
+//        g_character.append('rect')
+//          .attr('x', 0)
+//          .attr('y', 0)
+//          .attr('width', site_scale(1))
+//          .attr('height', site_scale(1))
+//          .attr('fill', colors[character] || 'red')
+//        
+//        g_character.append('text')
+//          .attr('transform', 'translate(' + site_scale(.5) + ',' + site_scale(.5) + ')')
+//          .attr('text-anchor', 'middle')
+//          .attr('dy', '.25em')
+//          .text(character)
+//      }
+//    }
   }
   componentDidMount(){
     if(this.props.fasta){
@@ -90,7 +111,10 @@ class Alignment extends Component {
     }
   }
   render(){
-    return (<div style={{width: "100%", "overflowX": "scroll"}}>
+    return (<div 
+        style={{width: "100%", "overflowX": "scroll"}}
+        onScroll={(e)=>console.log('scrolling that div', e)}
+      >
       <svg id="alignment"></svg>
     </div>);
   }
--- src/fasta.js
+++ src/fasta.js
@@ -1,30 +1,37 @@
 function fastaParser(fasta) {
   const lines = fasta.split('\n');
 
-  var sequences = [],
-    seqrecord = {},
+  var characters = [],
+    names = [],
     seq,
+    name,
     line;
 
   for (var i = 0; i < lines.length; i++){
     line = lines[i];
     if (line[0] == '>'){
       if(i != 0) {
-        seqrecord.seq = seq;
-        sequences.push(seqrecord)
+        characters = characters.concat(
+          seq.split('')
+            .map((char,i) => {return {char: char, j:i, i:names.length, name:name};})
+        );
       }
-      seqrecord = { 
-        name: line.slice(1)
-      };
+      name = line.slice(1);
+      names.push(name);
       seq = '';
     } else {
       seq += line;
     }
   }
 
-  seqrecord.seq = seq;
-  sequences.push(seqrecord);
-  return sequences;
+  characters = characters.concat(
+    seq.split('')
+      .map((char,i) => {return {char: char, j:i, i:names.length, name:name};})
+  );
+  return { 
+    characters: characters,
+    names: names
+  };
 }
 
 module.exports = fastaParser;
