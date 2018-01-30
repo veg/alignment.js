69872a25e3c10b05e2e169a8ab0a6a895d9f8663
Stephen Shank <sshank314@gmail.com>
2018-01-30 17:25:45 -0500

    Make suitable for NPM.

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,10 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
       },
       {
         test: /\.css$/,
