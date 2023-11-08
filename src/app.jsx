40e28cd7b98f77c83ddeafbad4d4f61af74be450
Stephen Shank <sshank314@gmail.com>
2023-11-08 14:23:55 -0500

    Update and prepare for refactoring.

--- src/app.jsx
+++ src/app.jsx
@@ -1,113 +1,15 @@
-import "./styles.scss";
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, NavLink } from "react-router-dom";
+import React, { Component } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import Home from "./app/home.jsx";
-import * as FASTA from "./app/FASTA.jsx";
-import * as FNA from "./app/FNA.jsx";
-import * as BAM from "./app/BAM.jsx";
-import PreventDefaultPatch from "./prevent_default_patch";
-
-function Divider(props) {
-  return [
-    <div className="dropdown-divider" key="divider-div" />,
-    props.header ? (
-      <h6 className="dropdown-header" key="divider-header">
-        {props.header}
-      </h6>
-    ) : null
-  ];
-}
-
-function Link(props) {
-  return (
-    <NavLink className="dropdown-item link" to={props.to}>
-      {props.header}
-    </NavLink>
-  );
-}
-
-function Dropdown(props) {
-  return (
-    <ul className="navbar-nav ">
-      <li className="nav-item dropdown">
-        <a
-          className="nav-link dropdown-toggle"
-          href="#"
-          id="navbarDropdown"
-          role="button"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-        >
-          {props.title}
-        </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          {props.children}
-        </div>
-      </li>
-    </ul>
-  );
-}
-
-function FASTALinks(props) {
-  return (
-    <Dropdown title={"FASTA"}>
-      <Link to="/fasta-viewer" header="Viewer" />
-      <Divider header="Examples" />
-      <Link to="/fasta-aminoacid" header="Amino acid alignment" />
-      <Link to="/fasta-highlight" header="Highlight individual sites" />
-      <Link to="/fasta-start" header="Start at a given sequence and site" />
-      <Link to="/fasta-lowercase" header="Lower case alignment" />
-      <Link to="/fasta-svg" header="SVG alignment" />
-      <Link to="/fasta-intrahost" header="Intrahost variation" />
-      <Link to="/fasta-sequence-bar" header="Sequence Bar Chart" />
-      <Link to="/fasta-line" header="Site Line Chart" />
-      <Link to="/fasta-site-stacked-bar" header="Site Stacked Bar Chart" />
-      <Link to="/fasta-ar" header="Artificial Recombination" />
-      <Link to="/fasta-click-and-hover" header="Click handler" />
-    </Dropdown>
-  );
-}
-
-function FNALinks(props) {
-  return (
-    <Dropdown title={"FNA"}>
-      <Link to="/fna-viewer" header="Viewer" />
-      <Divider header="Examples" />
-      <Link to="/fna-immunology" header="Immunology - heavy chain regions" />
-      <Link to="/fna-hiv" header="HIV - site annotations" />
-      <Link to="/fna-basesvgtree" header="BaseSVGTreeInstance" />
-    </Dropdown>
-  );
-}
-
-function BAMLinks(props) {
-  return (
-    <Dropdown title={"BAM"}>
-      <Link to="/sam-viewer" header="Viewer" />
-      <Divider header="Examples" />
-      <Link to="/sam-variantcaller" header="Variant caller" />
-      <Link to="/sam-scaffold" header="Scaffold viewer" />
-    </Dropdown>
-  );
-}
+import './styles.scss';
 
 function NavBar(props) {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink className="navbar-brand" to="/">
-          Javascript Alignment Viewer
-        </NavLink>
-        <div className="collapse navbar-collapse">
-          <FASTALinks />
-          <FNALinks />
-          <BAMLinks />
-        </div>
-      </nav>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Alignment UI</a>
+      </div>
+    </nav>
   );
 }
 
@@ -118,44 +20,12 @@ class App extends Component {
       data: null
     };
   }
-
   render() {
     return (
       <div>
         <NavBar />
         <div style={{ maxWidth: 1140 }} className="container-fluid">
-          <Route exact path="/" render={props => <Home />} />
-
-          <Route path="/fasta-viewer" render={props => <FASTA.FASTAViewer />} />
-          <Route path="/fasta-aminoacid" component={FASTA.AminoAcid} />
-          <Route path="/fasta-highlight" component={FASTA.Highlight} />
-          <Route path="/fasta-start" component={FASTA.StartAtSiteAndSequence} />
-          <Route path="/fasta-lowercase" component={FASTA.Lowercase} />
-          <Route path="/fasta-svg" component={FASTA.SVGAlignmentExample} />
-          <Route path="/fasta-intrahost" component={FASTA.Intrahost} />
-          <Route path="/fasta-line" component={FASTA.SiteLineChart} />
-          <Route
-            path="/fasta-sequence-bar"
-            component={FASTA.SequenceBarChart}
-          />
-          <Route
-            path="/fasta-site-stacked-bar"
-            component={FASTA.SiteStackedBarChart}
-          />
-          <Route path="/fasta-ar" component={FASTA.ArtificialRecombination} />
-          <Route
-            path="/fasta-click-and-hover"
-            component={FASTA.ClickAndHover}
-          />
-
-          <Route path="/fna-viewer" component={FNA.FNAViewer} />
-          <Route path="/fna-immunology" component={FNA.Immunology} />
-          <Route path="/fna-hiv" component={FNA.HIV} />
-          <Route path="/fna-basesvgtree" component={FNA.BaseSVGTreeInstance} />
-
-          <Route path="/sam-viewer" component={BAM.BAMViewer} />
-          <Route path="/sam-scaffold" component={BAM.ScaffoldExample} />
-          <Route path="/sam-variantcaller" component={BAM.VariantCaller} />
+          <button>a button</button>
         </div>
       </div>
     );
@@ -164,14 +34,12 @@ class App extends Component {
 
 function Main(props) {
   return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   );
 }
 
-PreventDefaultPatch(document);
-ReactDOM.render(
-  <Main />,
-  document.body.appendChild(document.createElement("div"))
-);
+//PreventDefaultPatch(document);
+const root_div = document.body.appendChild(document.createElement("div")),
+  root = createRoot(root_div);
+root.render(<Main />);
+
---
+++ webpack.config.cjs
@@ -0,0 +1,61 @@
+const path = require('path'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  entry: {
+    main: path.resolve('src', 'app.jsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React/SASS webapp and library'
+    }),
+  ].concat(devMode ? [] : [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    })
+  ]),
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  mode: devMode ? 'development' : 'production',
+  devServer: {
+    liveReload: true,
+    hot: false
+  },
+  devtool: devMode ? 'eval-cheap-module-source-map' : false,
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: 'vendor'
+    }
+  }
+};
